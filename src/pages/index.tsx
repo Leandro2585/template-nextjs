42f3a2b77d228ef3fc77a6928e3368adda1969b8
Leandro2585 <leo.real2585@gmail.com>
2020-11-17 18:43:11 -0300

    babel config

---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h2>Hello World</h2>
+      </main>
+
+    </div>
+  )
+}
+
+export default Home;
---
+++ src/styles/theme.ts
@@ -0,0 +1,9 @@
+const theme = {
+  colors: {
+    text: '#000',
+    background: '#e1e1e6',
+    primary: '#222',
+  },
+};
+
+export default theme;
